b114a885a4487d8fda07db01be9a4e8506981d09
vi-ii-mmvii <nur.adina.muratkyzy@gmail.com>
2025-01-21 20:42:33 +0500

    Update hw.js

--- hw.js
+++ hw.js
@@ -1,27 +1,14 @@
 
-//hw3.1
-function calcFr(celsius) {
-  let fr = (celsius * 9 / 5) + 32;
-  console.log(`Температура ${celsius}\u00B0C в градусах Фаренгейта: ${fr.toFixed(2)} \u00B0F`);
-}
 
-calcFr(5);
-calcFr(73.5555);
-calcFr('1');
-calcFr(6456.87789);
-
-
- let temperature=7; //global scope
+ let temperature=7; // is global scope
 function calcC(fr) {
-  let celsius = (fr - 32) * 5 / 9;
+  let celsius = (fr - 32) * 5 / 9; // is function scope
   console.log(`Температура ${fr}\u00B0F в градусах Цельсия: ${celsius.toFixed(2)} \u00B0C`);
   if (true) {
-    let x = 10; // Variable x has block scope
-    console.log(x); // Output: 10
+    let x = 10; // is block scope
+    console.log(x); // will work only in block 
   }
 }
-console.log(temperature);
-console.log(celsius);
-calcC(34);
-calcC(43.7);
-calcC(84.867);
+console.log(temperature); // will work
+console.log(celsius); // would not, because it was created in function 
+
